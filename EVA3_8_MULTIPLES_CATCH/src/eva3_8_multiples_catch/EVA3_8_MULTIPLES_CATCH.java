ad18187751b0b65db55bf40f2ec7c100288f218a
José Sebastian López Ibarra <99367521+SebastianLopez43@users.noreply.github.com>
2022-05-15 19:26:27 -0500

    Práctica 8_Multiples_Catch

---
+++ EVA3_8_MULTIPLES_CATCH/src/eva3_8_multiples_catch/EVA3_8_MULTIPLES_CATCH.java
@@ -0,0 +1,36 @@
+package eva3_8_multiples_catch;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * @author José Sebastian López Ibarra
+ * Tuesday May 10 2022
+ */
+
+public class EVA3_8_MULTIPLES_CATCH {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1 = 0, num2 = 0;
+        
+        try { // Código que puede fallar.
+            System.out.println("Introduce un número entero: ");
+            num1 = input.nextInt();
+        
+            System.out.println("Introduce otro número entero: ");
+            num2 = input.nextInt();
+            
+            System.out.println("\nDivisión: " + (num1/num2));
+        } catch (ArithmeticException e) { // Lo que sucede si se produce una expeción.
+            System.out.println("Se produjo una división entre cero");
+        } catch (InputMismatchException e) {
+            System.out.println("Introduce un número válido.");
+        }
+        
+        System.out.println("\nFin del programa.");
+    }    
+}
